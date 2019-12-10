593bd59c4673c11a7e7d0f99d3a9c3c271e61309
Kevin Davis <Kevindavis5454@gmail.com>
2019-12-09 20:40:31 -0700

    added new Project

--- projects.js
+++ projects.js
@@ -1,5 +1,7 @@
+
 function quizAppStart() {
     $('#quiz-app-button').on('click', function(event) {
+        event.preventDefault();
         quizAppDescription();
     });
     console.log('`quizAppStart` ran');
@@ -21,10 +23,32 @@ function quizAppDescription() {
     console.log('`quizAppDescription` ran');
 }
 
+function sproutAppStart () {
+    $('#sprouts-button').on('click', function(event){
+        event.preventDefault();
+        sproutsDescritpion();
+
+    });
+}
+
+function sproutsDescritpion() {
+    const sproutsAppInfo = $(`
+    <div class="project-info" aria-live="assertive">
+                    <h2>Sprouts and Showers</h2>
+                    <p>Uses HTML5, CSS, Javascript, JQuery</p>
+                    <a href="https://kevindavis5454.github.io/sprouts-and-showers/">Live Page</a>
+                    <a href="https://github.com/Kevindavis5454/sprouts-and-showers.git">Repo</a>
+
+                    <img class="project-pic" src="https://Kevindavis5454.github.io/portfolio-greybox/assets/sprouts-landing-page.png" alt="Screen shot of Sprouts and Showers App">
+    `)
+    $('#project-description-block').html(sproutsAppInfo);
+}
+
 
 
 function handleProjectDescriptions() {
     quizAppStart();
+    sproutAppStart()
 }
 
 $(handleProjectDescriptions);
