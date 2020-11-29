322ab68cff370c55fea18c792a0f39a9c362753d
Kevin Davis <Kevindavis5454@gmail.com>
2020-11-29 09:13:50 -0700

    added Projects

--- projects.js
+++ projects.js
@@ -41,9 +41,73 @@ function sproutsDescritpion() {
   $("#project-description-block").html(sproutsAppInfo);
 }
 
+function notefulAppStart() {
+  $("#noteful-button").on("click", function (event) {
+    event.preventDefault();
+    notefulDescritpion();
+  });
+}
+
+function notefulDescritpion() {
+  const notefulAppInfo = $(`
+    <div class="project-info" aria-live="assertive">
+                    <h2>Noteful</h2>
+                    <p>Uses HTML5, CSS, Javascript, React, Node.js, Express, PostgreSQL, Knex </p>
+                    <a href="https://noteful-beta.now.sh/">Live Page</a>
+                    <a href="https://github.com/Kevindavis5454/noteful.git">Repo</a>
+
+                    <img class="project-pic" src="https://Kevindavis5454.github.io/portfolio/assets/noteful-screen.png" alt="Screen shot of Noteful App">
+    `);
+  $("#project-description-block").html(notefulAppInfo);
+}
+
+function cuththroatAppStart() {
+  $("#cut-losers-button").on("click", function (event) {
+    event.preventDefault();
+    cutthroatDescritpion();
+  });
+}
+
+function cuththroatDescritpion() {
+  const cuththroatAppInfo = $(`
+    <div class="project-info" aria-live="assertive">
+                    <h2>Cutthroat Losers</h2>
+                    <p>Uses HTML5, CSS, Javascript, React, Node.js, Express, PostgreSQL, Knex </p>
+                    <a href="https://cutthroat-losers-app.herokuapp.com/">Live Page</a>
+                    <a href="https://github.com/Kevindavis5454/cutthroat-losers.git">APP Repo</a>
+                    <a href="https://github.com/LadyLudo/KT_CutthroatLosers-api.git">API Repo</a>
+
+                    <img class="project-pic" src="https://Kevindavis5454.github.io/portfolio/assets/CLosers1.png" alt="Screen shot of Cutthroat Losers App">
+    `);
+  $("#project-description-block").html(cuththroatAppInfo);
+}
+
+function tinyLunchesAppStart() {
+  $("#tiny-lunches-buttons").on("click", function (event) {
+    event.preventDefault();
+    tinyLunchesDescritpion();
+  });
+}
+
+function tinyLunchesDescritpion() {
+  const tinyLunchesAppInfo = $(`
+    <div class="project-info" aria-live="assertive">
+                    <h2>Tiny Lunches</h2>
+                    <p>Uses HTML5, CSS, Javascript, React, Node.js, Express, PostgreSQL, Knex </p>
+                    <a href="https://tiny-lunches-app.vercel.app/">Live Page</a>
+                    <a href="https://github.com/Kevindavis5454/tiny-lunches-app.git">APP Repo</a>
+                    <a href="https://github.com/LadyLudo/tinylunchesapi.git">API Repo</a>
+
+                    <img class="project-pic" src="https://Kevindavis5454.github.io/portfolio/assets/TinyLunches.png" alt="Screen shot of Tiny Lunches App">
+    `);
+  $("#project-description-block").html(tinyLunchesAppInfo);
+}
+
 function handleProjectDescriptions() {
   quizAppStart();
   sproutAppStart();
+  notefulAppStart();
+  cuththroatAppStart();
 }
 
 $(handleProjectDescriptions);
